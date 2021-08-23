d46d8d7f484ba367e618d2696d5094431cc1aefc
taabishhh <taabishsutriwala@gmail.com>
2021-08-23 16:18:18 +0530

    onboarding added

---
+++ lib/screens/onboarding/onboarding.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class OnBoarding extends StatefulWidget {
+  OnBoarding({Key? key}) : super(key: key);
+
+  @override
+  _OnBoardingState createState() => _OnBoardingState();
+}
+
+class _OnBoardingState extends State<OnBoarding> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+       child: null,
+    );
+  }
+}
---
+++ lib/screens/onboarding/showloading.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ShowLoading extends StatefulWidget {
+  ShowLoading({Key? key}) : super(key: key);
+
+  @override
+  _ShowLoadingState createState() => _ShowLoadingState();
+}
+
+class _ShowLoadingState extends State<ShowLoading> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+       child: null,
+    );
+  }
+}
