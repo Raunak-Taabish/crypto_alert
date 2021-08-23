56afed707ff19e4e4c72e274c73c62a4717b3e97
taabishhh <taabishsutriwala@gmail.com>
2021-08-23 16:14:17 +0530

    Login_register added

---
+++ lib/screens/authentication/login_register.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Login_Register extends StatefulWidget {
+  Login_Register({Key? key}) : super(key: key);
+
+  @override
+  _Login_RegisterState createState() => _Login_RegisterState();
+}
+
+class _Login_RegisterState extends State<Login_Register> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+       child: null,
+    );
+  }
+}
