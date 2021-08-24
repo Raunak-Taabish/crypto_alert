dfc1e82b1fd5c25611bab0fc8cc06b480c7ea12b
taabishhh <taabishsutriwala@gmail.com>
2021-08-25 00:12:48 +0530

    API integrated

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -40,7 +40,7 @@ class _HomeState extends State<Home> {
         //   articleUrl: element["url"],
         // );
         // news.add(article);
-      }); 
+      });
       return names;
     }
   }
