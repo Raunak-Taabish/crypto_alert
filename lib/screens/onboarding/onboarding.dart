db3c691e499f310d42e7802727b55e86e6b43cf1
raunak singh <68766724+Raunaks068619@users.noreply.github.com>
2021-08-25 11:48:20 +0530

    onboarding code optimize

--- lib/screens/onboarding/onboarding.dart
+++ lib/screens/onboarding/onboarding.dart
@@ -12,6 +12,85 @@ class OnBoarding extends StatefulWidget {
 class _OnBoardingState extends State<OnBoarding> {
   PageController pageController = PageController();
   int flag = 0;
+
+  Scaffold pages(String image, String btext, String text) {
+    return Scaffold(
+      backgroundColor: Color(0xFF151515),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(25, 30, 25, 40),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                children: [
+                  Column(
+                    children: [
+                      Image.asset(
+                        "assets/images/logoname.png",
+                        width: MediaQuery.of(context).size.width / 2,
+                      ),
+                      const SizedBox(height: 40),
+                      Image.asset(
+                        "assets/images/$image",
+                        width: 180,
+                      ),
+                      const SizedBox(
+                        height: 50,
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "$btext",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontFamily: 'Montserrat Alternates',
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                          Text(
+                            "$text",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontFamily: 'Montserrat Alternates',
+                              //fontWeight: FontWeight.w600,
+                            ),
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              Container(
+                width: double.infinity,
+                color: Colors.transparent,
+                child: Align(
+                  alignment: Alignment.bottomRight,
+                  child: FloatingActionButton(
+                    backgroundColor: Colors.white,
+                    onPressed: () {
+                      setState(() {
+                        pageController.animateToPage(++flag,
+                            duration: const Duration(milliseconds: 500),
+                            curve: Curves.easeIn);
+                      });
+                    },
+                    child: const Icon(
+                      Icons.arrow_forward_ios,
+                      color: Color(0xFF151515),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return PageView(
@@ -24,232 +103,173 @@ class _OnBoardingState extends State<OnBoarding> {
         print(flag);
       },
       children: [
-        Scaffold(
-          backgroundColor: Color(0xFF151515),
-          body: SafeArea(
-            child: Padding(
-              padding: const EdgeInsets.fromLTRB(25, 30, 25, 40),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
-                    children: [
-                      Column(
-                        children: [
-                          Image.asset(
-                            "assets/images/logoname.png",
-                            width: MediaQuery.of(context).size.width / 2,
-                          ),
-                          const SizedBox(height: 40),
-                          Image.asset(
-                            "assets/images/search.png",
-                            width: 180,
-                          ),
-                          const SizedBox(
-                            height: 50,
-                          ),
-                          RichText(
-                            text: const TextSpan(
-                              // Note: Styles for TextSpans must be explicitly defined.
-                              // Child text spans will inherit styles from parent
-                              style: TextStyle(
-                                fontSize: 16.0,
-                                color: Colors.white,
-                              ),
-                              children: <TextSpan>[
-                                TextSpan(
-                                    text: 'Search',
-                                    style: TextStyle(
-                                      fontWeight: FontWeight.w600,
-                                      fontFamily: 'Montserrat Alternates',
-                                    )),
-                                TextSpan(
-                                  text: ' the crypto u \n     wanna follow',
-                                ),
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                  Container(
-                    width: double.infinity,
-                    color: Colors.transparent,
-                    child: Align(
-                      alignment: Alignment.bottomRight,
-                      child: FloatingActionButton(
-                        backgroundColor: Colors.white,
-                        onPressed: () {
-                          setState(() {
-                            pageController.animateToPage(++flag,
-                                duration: const Duration(milliseconds: 500),
-                                curve: Curves.easeIn);
-                          });
-                        },
-                        child: const Icon(
-                          Icons.arrow_forward_ios,
-                          color: Color(0xFF151515),
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
+        pages(
+          'search.png',
+          'Search',
+          'the crypto u\nwanna follow',
         ),
-        Scaffold(
-          backgroundColor: Color(0xFF151515),
-          body: SafeArea(
-            child: Padding(
-              padding: const EdgeInsets.fromLTRB(25, 30, 25, 40),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
-                    children: [
-                      Column(
-                        children: [
-                          Image.asset(
-                            "assets/images/logoname.png",
-                            width: MediaQuery.of(context).size.width / 2,
-                          ),
-                          const SizedBox(height: 40),
-                          Image.asset(
-                            "assets/images/favorite.png",
-                            width: 180,
-                          ),
-                          const SizedBox(
-                            height: 50,
-                          ),
-                          RichText(
-                            text: const TextSpan(
-                              // Note: Styles for TextSpans must be explicitly defined.
-                              // Child text spans will inherit styles from parent
-                              style: TextStyle(
-                                fontSize: 16.0,
-                                color: Colors.white,
-                              ),
-                              children: <TextSpan>[
-                                TextSpan(
-                                    text: 'Add',
-                                    style: TextStyle(
-                                      fontWeight: FontWeight.w600,
-                                      fontFamily: 'Montserrat Alternates',
-                                    )),
-                                TextSpan(
-                                  text: ' your crypto to \n     favorites list',
-                                ),
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                  Container(
-                    width: double.infinity,
-                    color: Colors.transparent,
-                    child: Align(
-                      alignment: Alignment.bottomRight,
-                      child: FloatingActionButton(
-                        backgroundColor: Colors.white,
-                        onPressed: () {
-                          setState(() {
-                            pageController.animateToPage(++flag,
-                                duration: const Duration(milliseconds: 500),
-                                curve: Curves.easeIn);
-                          });
-                        },
-                        child: const Icon(
-                          Icons.arrow_forward_ios,
-                          color: Color(0xFF151515),
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
+        pages(
+          'favorite.png',
+          'Add',
+          'your crypto to\nfavorites list',
         ),
-        Scaffold(
-          backgroundColor: Color(0xFF151515),
-          body: SafeArea(
-            child: Padding(
-              padding: const EdgeInsets.fromLTRB(25, 30, 25, 40),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
-                    children: [
-                      Column(
-                        children: [
-                          Image.asset(
-                            "assets/images/logoname.png",
-                            width: MediaQuery.of(context).size.width / 2,
-                          ),
-                          const SizedBox(height: 40),
-                          Image.asset(
-                            "assets/images/binoculars.png",
-                            width: 180,
-                          ),
-                          const SizedBox(
-                            height: 50,
-                          ),
-                          RichText(
-                            text: const TextSpan(
-                              // Note: Styles for TextSpans must be explicitly defined.
-                              // Child text spans will inherit styles from parent
-                              style: TextStyle(
-                                fontSize: 16.0,
-                                color: Colors.white,
-                              ),
-                              children: <TextSpan>[
-                                TextSpan(
-                                    text: 'Follow',
-                                    style: TextStyle(
-                                      fontWeight: FontWeight.w600,
-                                      fontFamily: 'Montserrat Alternates',
-                                    )),
-                                TextSpan(
-                                  text:
-                                      ' your crypto to \n    get latest updates',
-                                ),
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                  Container(
-                    width: double.infinity,
-                    color: Colors.transparent,
-                    child: Align(
-                      alignment: Alignment.bottomRight,
-                      child: FloatingActionButton(
-                        backgroundColor: Colors.white,
-                        onPressed: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (BuildContext context) =>
-                                      ShowLoading()));
-                        },
-                        child: const Icon(
-                          Icons.arrow_forward_ios,
-                          color: Color(0xFF151515),
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
+        pages(
+          'binoculars.png',
+          'Follow',
+          'your crypto to\nget latest updates',
         ),
+
+        // Scaffold(
+        //   backgroundColor: Color(0xFF151515),
+        //   body: SafeArea(
+        //     child: Padding(
+        //       padding: const EdgeInsets.fromLTRB(25, 30, 25, 40),
+        //       child: Column(
+        //         mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        //         children: [
+        //           Column(
+        //             children: [
+        //               Column(
+        //                 children: [
+        //                   Image.asset(
+        //                     "assets/images/logoname.png",
+        //                     width: MediaQuery.of(context).size.width / 2,
+        //                   ),
+        //                   const SizedBox(height: 40),
+        //                   Image.asset(
+        //                     "assets/images/favorite.png",
+        //                     width: 180,
+        //                   ),
+        //                   const SizedBox(
+        //                     height: 50,
+        //                   ),
+        //                   RichText(
+        //                     text: const TextSpan(
+        //                       // Note: Styles for TextSpans must be explicitly defined.
+        //                       // Child text spans will inherit styles from parent
+        //                       style: TextStyle(
+        //                         fontSize: 16.0,
+        //                         color: Colors.white,
+        //                       ),
+        //                       children: <TextSpan>[
+        //                         TextSpan(
+        //                             text: 'Add',
+        //                             style: TextStyle(
+        //                               fontWeight: FontWeight.w600,
+        //                               fontFamily: 'Montserrat Alternates',
+        //                             )),
+        //                         TextSpan(
+        //                           text: ' your crypto to \n     favorites list',
+        //                         ),
+        //                       ],
+        //                     ),
+        //                   ),
+        //                 ],
+        //               ),
+        //             ],
+        //           ),
+        //           Container(
+        //             width: double.infinity,
+        //             color: Colors.transparent,
+        //             child: Align(
+        //               alignment: Alignment.bottomRight,
+        //               child: FloatingActionButton(
+        //                 backgroundColor: Colors.white,
+        //                 onPressed: () {
+        //                   setState(() {
+        //                     pageController.animateToPage(++flag,
+        //                         duration: const Duration(milliseconds: 500),
+        //                         curve: Curves.easeIn);
+        //                   });
+        //                 },
+        //                 child: const Icon(
+        //                   Icons.arrow_forward_ios,
+        //                   color: Color(0xFF151515),
+        //                 ),
+        //               ),
+        //             ),
+        //           ),
+        //         ],
+        //       ),
+        //     ),
+        //   ),
+        // ),
+        // Scaffold(
+        //   backgroundColor: Color(0xFF151515),
+        //   body: SafeArea(
+        //     child: Padding(
+        //       padding: const EdgeInsets.fromLTRB(25, 30, 25, 40),
+        //       child: Column(
+        //         mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        //         children: [
+        //           Column(
+        //             children: [
+        //               Column(
+        //                 children: [
+        //                   Image.asset(
+        //                     "assets/images/logoname.png",
+        //                     width: MediaQuery.of(context).size.width / 2,
+        //                   ),
+        //                   const SizedBox(height: 40),
+        //                   Image.asset(
+        //                     "assets/images/binoculars.png",
+        //                     width: 180,
+        //                   ),
+        //                   const SizedBox(
+        //                     height: 50,
+        //                   ),
+        //                   RichText(
+        //                     text: const TextSpan(
+        //                       // Note: Styles for TextSpans must be explicitly defined.
+        //                       // Child text spans will inherit styles from parent
+        //                       style: TextStyle(
+        //                         fontSize: 16.0,
+        //                         color: Colors.white,
+        //                       ),
+        //                       children: <TextSpan>[
+        //                         TextSpan(
+        //                             text: 'Follow',
+        //                             style: TextStyle(
+        //                               fontWeight: FontWeight.w600,
+        //                               fontFamily: 'Montserrat Alternates',
+        //                             )),
+        //                         TextSpan(
+        //                           text:
+        //                               ' your crypto to \n    get latest updates',
+        //                         ),
+        //                       ],
+        //                     ),
+        //                   ),
+        //                 ],
+        //               ),
+        //             ],
+        //           ),
+        //           Container(
+        //             width: double.infinity,
+        //             color: Colors.transparent,
+        //             child: Align(
+        //               alignment: Alignment.bottomRight,
+        //               child: FloatingActionButton(
+        //                 backgroundColor: Colors.white,
+        //                 onPressed: () {
+        //                   Navigator.push(
+        //                       context,
+        //                       MaterialPageRoute(
+        //                           builder: (BuildContext context) =>
+        //                               ShowLoading()));
+        //                 },
+        //                 child: const Icon(
+        //                   Icons.arrow_forward_ios,
+        //                   color: Color(0xFF151515),
+        //                 ),
+        //               ),
+        //             ),
+        //           ),
+        //         ],
+        //       ),
+        //     ),
+        //   ),
+        // ),
       ],
     );
   }
